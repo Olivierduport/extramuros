1d6ca465ca94fafcef38a2de3c5a2fc594684526
Olivierduport <64253108+Olivierduport@users.noreply.github.com>
2020-06-09 15:15:38 +0200

    Add files via upload

--- forms.php
+++ forms.php
@@ -2,184 +2,198 @@
 
 <?php 
 echo $_POST['submit'];
-if(isset($_POST['submit'])){
-    $to = "extramurosresidency@gmail.com"; 
-    $from = $_POST['email'];
+if(isset($_POST['submit']))
+  // upload
+
+
+  // mail
+{
+    $to = $_POST['email'];
+    $from = "test@extramuros.olivierduport.fr"; 
     $first_name = $_POST['firstname'];
     $last_name = $_POST['lastname'];
     $artist_name = $_POST['artistname'];
     $age = $_POST['age'];
+    $address = $_POST['address'];
+    $country = $_POST['country'];
+    $city = $_POST['city'];
+    $zipcode = $_POST['zipcode'];
+    $email = $_POST['email'];
+    $website = $_POST['website'];
+    $label = $_POST['label'];
+    $phone = $_POST['phone'];
+    $citizenship = $_POST['citizenship'];
+    $propo = $_POST['propo'];
+    $link1 = $_POST['link1'];
+    $link2 = $_POST['link2'];
+    $file1 = $_POST['lfile1'];
+    $file2 = $_POST['file2'];
     $subject = "New application to Extra Muros 2019";
     $subject2 = "Your copy of your Extra Muros 2019 application";
-    $headers = "From: " .$_POST['email']. "\r\n";
-  $headers = "Reply-To: " .$to. "\r\n";
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers = "Content-Type: text/html; charset=UTF-8\r\n";
 
+  	$headers = "MIME-Version: 1.0\r\n";
+  	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $headers .= "From: " .$from. "\r\n";
 
+  	$headers2 = "MIME-Version: 1.0\r\n";
+  	$headers2 .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $headers2 .= "From: " .$to. "\r\n";
 
-  
+    // Message1
 
-  $message =
- '<h1><strong>Thank you for you application</strong><br>
-    <br>
-  Here is a copy of the data submitted</h1>
+$message = '<html><body>';
+ $message .= '<h1><strong>Thank you for you application</strong><br>
+    <br>Here is a copy of the data submitted</h1>';
 
+$message .= '<img src="http://extramuros.olivierduport.fr/logo.png" width="300" length="300" alt="Extra Muros" />';
 
-  <style>
+$message .= '<style>
     p {
       color: white;
       background-color: blue;
       padding: 15px;
       border: 1px solid black;
     }
-  </style> 
+  </style>';
+  $message .= '
     <p>
       <h2>
       <div class="applyForm><label for="title>Identity</label>
         <br>
-      </h2>
+      </h2>';
       
-<table>
-    <thead>
-        <tr>
-            <th colspan="2"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td><label for="name">Artist Name</label></td>
-            <td>'. $_POST['lastname'] .'</td>
-        </tr>
-        <tr>
-            <td><label for="firstname">First Name</label></td>
-            <td>'.$first_name.'</td>
-        </tr>
-        <tr>
-            <td><label for="lastname">Last Name</label></td>
-            <td><input type="text" name="lastname" id="lastname" required></td>
-        </tr>
-        <tr>
-          <td><label for="age">Age</label></td>
-          <td><input type="text" name="age" id="age" required></td>
-        </tr>
-    </tbody>
-</table>
-</p>
-  </div>
+$message .='<table>';
+$message .= "<tr><td><strong>Artist Name:</strong> </td><td>" . $_POST['artistname'] . "</td></tr>";
+$message .= "<tr><td><strong>First Name:</strong> </td><td>" . $_POST['firstname'] . "</td></tr>";
+$message .= "<tr><td><strong>Last Name:</strong> </td><td>" . strip_tags($_POST['lastname']) . "</td></tr>";
+$message .= "<tr><td><strong>Age:</strong> </td><td>" . strip_tags($_POST['age']) . "</td></tr>";
+$message .= "</table>";
 
-
-  <p>
+$message .= '
+    <p>
       <h2>
       <div class="applyForm><label for="title>Details</label>
         <br>
-      </h2>
+      </h2>';
+      
+$message .='<table>';
+$message .= "<tr><td><strong>Address:</strong> </td><td>" . $_POST['address'] . "</td></tr>";
+$message .= "<tr><td><strong>Country:</strong> </td><td>" . strip_tags($_POST['country']) . "</td></tr>";
+$message .= "<tr><td><strong>City:</strong> </td><td>" . strip_tags($_POST['city']) . "</td></tr>";
+$message .= "<tr><td><strong>Zip Code:</strong> </td><td>" . strip_tags($_POST['zipcode']) . "</td></tr>";
+$message .= "<tr><td><strong>Citizenship:</strong> </td><td>" . strip_tags($_POST['citizenship']) . "</td></tr>";
+$message .= "</table>";
 
-<table>
-    <thead>
-        <tr>
-            <th colspan="2"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-          <td><label for="title">Address</label></td>
-          <td><input type="text" name="address" id="address" required></td>
-        </tr>
-        <tr>
-          <td><label for="name">Country</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-         <tr>
-          <td><label for="name">City</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-         <tr>
-          <td><label for="name">Zip Code</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr> 
-        <tr>
-          <td><label for="name">Citizenship</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-    </tbody>
-</table>
-</p>
-  </div>
-
+$message .= '
     <p>
       <h2>
       <div class="applyForm><label for="title>Contact</label>
         <br>
-      </h2>
+      </h2>';
+      
+$message .='<table>';
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . $_POST['email'] . "</td></tr>";
+$message .= "<tr><td><strong>Website:</strong> </td><td>" . $_POST['website'] . "</td></tr>";
+$message .= "<tr><td><strong>Label:</strong> </td><td>" . $_POST['label'] . "</td></tr>";
+$message .= "<tr><td><strong>Phone:</strong> </td><td>" . $phone . "</td></tr>";
+$message .= "</table>";
 
-<table>
-    <thead>
-        <tr>
-            <th colspan="2"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-          <td><label for="title">E-mail</label></td>
-          <td><input type="text" name="address" id="address" required></td>
-        </tr>
-        <tr>
-          <td><label for="name">Website</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-         <tr>
-          <td><label for="name">Label/Structure</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-         <tr>
-          <td><label for="name">Phone</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-    </tbody>
-</table>
-</p>
-  </div>
-
+$message .= '
     <p>
       <h2>
       <div class="applyForm><label for="title>Work</label>
         <br>
-      </h2>
+      </h2>';
+      
+$message .='<table>';
+$message .= "<tr><td><strong>Project Proposal:</strong> </td><td>" . strip_tags($_POST['propo']) . "</td></tr>";
+$message .= "<tr><td><strong>Links:</strong> </td><td>" . strip_tags($_POST['link1']) . "</td></tr>";
+$message .= "<tr><td><strong>File:</strong> </td><td>" . strip_tags($_POST['file1']) . "</td></tr>";
+$message .= "<tr><td><strong>Links:</strong> </td><td>" . strip_tags($_POST['link2']) . "</td></tr>";
+$message .= "</table>";
+$message .= "</br>";
+$message .= "If you are among qualified candidates, we will get in touch shortly. Thank you, again, for taking the time to apply to Extra Muros. 
+</br>FLEE";
+$message .= "</html>";
 
-<table>
-    <thead>
-        <tr>
-            <th colspan="2"></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-          <td><label for="title">Project Proposal (briefly detail why you want to be part of the Extra Muros program)</label></td>
-          <td><input type="text" name="address" id="address" required></td>
-        </tr>
-        <tr>
-          <td><label for="name">Link to previous work (audio or video recordings, scores, installations, etc.) 1</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-         <tr>
-          <td><label for="name">Link to previous work (audio or video recordings, scores, installations, etc.) 2</label></td>
-          <td><input type="text" name="name" id="name" required></td>
-        </tr>
-    </tbody>
-</table>
-</p>
-  </div>';
-
-  
+  // Message2
     
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+ $message2 = '<h1><strong>Thank you for you application</strong><br>
+    <br>Here is a copy of the data submitted</h1>';
 
+
+$message2 .= '<style>
+    p {
+      color: white;
+      background-color: blue;
+      padding: 15px;
+      border: 1px solid black;
+    }
+  </style>';
+  $message2 .= '
+    <p>
+      <h2>
+      <div class="applyForm><label for="title>Identity</label>
+        <br>
+      </h2>';
+      
+$message2 .='<table>';
+$message2 .= "<tr><td><strong>Artist Name:</strong> </td><td>" . $_POST['artistname'] . "</td></tr>";
+$message2 .= "<tr><td><strong>First Name:</strong> </td><td>" . strip_tags($_POST['firstname']) . "</td></tr>";
+$message2 .= "<tr><td><strong>Last Name:</strong> </td><td>" . strip_tags($_POST['lastname']) . "</td></tr>";
+$message2 .= "<tr><td><strong>Age:</strong> </td><td>" . strip_tags($_POST['age']) . "</td></tr>";
+$message2 .= "</table>";
+
+$message2 .= '
+    <p>
+      <h2>
+      <div class="applyForm><label for="title>Details</label>
+        <br>
+      </h2>';
+      
+$message2 .='<table>';
+$message2 .= "<tr><td><strong>Address:</strong> </td><td>" . $_POST['address'] . "</td></tr>";
+$message2 .= "<tr><td><strong>Country:</strong> </td><td>" . strip_tags($_POST['country']) . "</td></tr>";
+$message2 .= "<tr><td><strong>City:</strong> </td><td>" . strip_tags($_POST['city']) . "</td></tr>";
+$message2 .= "<tr><td><strong>Zip Code:</strong> </td><td>" . strip_tags($_POST['zipcode']) . "</td></tr>";
+$message2 .= "<tr><td><strong>Citizenship:</strong> </td><td>" . strip_tags($_POST['citizenship']) . "</td></tr>";
+$message2 .= "</table>";
+
+$message2 .= '
+    <p>
+      <h2>
+      <div class="applyForm><label for="title>Contact</label>
+        <br>
+      </h2>';
+      
+$message2 .='<table>';
+$message2 .= "<tr><td><strong>Email:</strong> </td><td>" . $_POST['email'] . "</td></tr>";
+$message2 .= "<tr><td><strong>Website:</strong> </td><td>" . $_POST['website'] . "</td></tr>";
+$message2 .= "<tr><td><strong>Label:</strong> </td><td>" . $_POST['label'] . "</td></tr>";
+$message2 .= "<tr><td><strong>Phone:</strong> </td><td>" . $phone . "</td></tr>";
+$message2 .= "</table>";
+
+$message2 .= '
+    <p>
+      <h2>
+      <div class="applyForm><label for="title>Work</label>
+        <br>
+      </h2>';
+      
+$message2 .='<table>';
+$message2 .= "<tr><td><strong>Project Proposal:</strong> </td><td>" . strip_tags($_POST['propo']) . "</td></tr>";
+$message2 .= "<tr><td><strong>Links:</strong> </td><td>" . strip_tags($_POST['link1']) . "</td></tr>";
+$message2 .= "<tr><td><strong>Links:</strong> </td><td>" . strip_tags($_POST['link2']) . "</td></tr>";
+$message2 .= "</table>";
+$message2 .= "</br>";
+$message2 .= "If you are among qualified candidates, we will get in touch shortly. Thank you, again, for taking the time to apply to Extra Muros. 
+</br>FLEE";
+$message2 .= "</html>";
     
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Application received, thank you1 " . $first_name . ", we will contact you shortly.";
+    echo "Application received, thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+}
 echo "<script>window.close();</script>";
 ?>
--- upload.php
+++ upload.php
@@ -1,28 +1,16 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
+//Get the uploaded file information
+$name_of_uploaded_file =
+    basename($_FILES['uploaded_file']['name']);
+
+//get the file extension of the file
+$type_of_uploaded_file =
+    substr($name_of_uploaded_file,
+    strrpos($name_of_uploaded_file, '.') + 1);
+
+$size_of_uploaded_file =
+    $_FILES["uploaded_file"]["size"]/1024;//size in KBsen été uploadé";
+
+
 ?>
